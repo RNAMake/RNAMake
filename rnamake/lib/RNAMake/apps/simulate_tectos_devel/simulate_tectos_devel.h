45b2f0c5ca36442d0194a26bed5e88b4cc6a9050
Joseph Yesselman <jyesselm@stanford.edu>
2015-11-06 16:49:19 -0800

    added a devel verision of simulate tectos

---
+++ rnamake/lib/RNAMake/apps/simulate_tectos_devel/simulate_tectos_devel.h
@@ -0,0 +1,51 @@
+//
+//  simulate_tectos.h
+//  RNAMake
+//
+//  Created by Joseph Yesselman on 5/18/15.
+//  Copyright (c) 2015 Joseph Yesselman. All rights reserved.
+//
+
+#ifndef __RNAMake__simulate_tectos__
+#define __RNAMake__simulate_tectos__
+
+#include <stdio.h>
+
+//RNAMake Headers
+#include "base/base.h"
+#include "motif_data_structures/motif_state_ensemble_tree.h"
+
+class SimulateTectos {
+public:
+    SimulateTectos(
+        String const &,
+        String const &,
+        String const &,
+        String const &);
+    
+public:
+    
+    void
+    run();
+
+private:
+    
+    //old code for comparision
+    MotifStateEnsembleTreeOP
+    get_mset_old(
+        String const &,
+        String const &,
+        String const &,
+        String const &);
+    
+    Strings
+    get_motifs_from_seq_and_ss(
+        String const &,
+        String const &);
+    
+    
+    
+    
+};
+
+#endif /* defined(__RNAMake__simulate_tectos__) */
