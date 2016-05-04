9242c877e2649dba46def4cd2a1f2cff6a637a67
Joseph Yesselman <jyesselm@stanford.edu>
2016-05-04 13:29:39 -0700

    finished updating comments and unittests for secondary structure

--- rnamake/unittests/secondary_structure_unittest.py
+++ rnamake/unittests/secondary_structure_unittest.py
@@ -97,9 +97,6 @@ class StructureUnittest(unittest.TestCase):
         #recycles chain ids does not run out of letters
         secondary_structure.Structure(sequence=seq, dot_bracket=db)
 
-
-
-
     def test_find_residue(self):
         ss = secondary_structure.Structure(sequence="AGCU+AGCU",
                                            dot_bracket="((((+))))")
@@ -111,12 +108,8 @@ class StructureUnittest(unittest.TestCase):
         if r2 is None:
             self.fail("did not find a known residue, find_residue not working")
 
-        try:
+        with self.assertRaises(exceptions.SecondaryStructureException):
             ss.get_residue(chain_id="A")
-        except ValueError:
-            pass
-        except:
-            self.fail("did not get expected error")
 
     def test_copy(self):
         ss = secondary_structure.Structure(sequence="AGCU+AGCU",
@@ -135,32 +128,6 @@ class StructureUnittest(unittest.TestCase):
         for r in ss.residues():
             if c_ss.get_residue(r.num, r.chain_id) is None:
                 self.fail("cannot find residue in to_str")
-
-    def _test_add_motif(self):
-        seq1 = 'CTAGGATATGGAAGATCCTCGGGAACGAGGATCTTCCTAAGTCCTAG'
-        seq2 = 'CTAGGAATCTGGAAGTACCGAGGAAACTCGGTACTTCCTGTGTCCTAG'
-
-        db1  = '(((((((..((((((((((((....))))))))))))...)))))))'
-        db2  = '((((((....((((((((((((....))))))))))))....))))))'
-
-        ss = ssfactory.factory.get_structure(seq1 +"+" + seq2, db1 + "+" + db2, to_RNA=1)
-
-        rm.manager.add_motif('resources/motifs/GAAA_tetraloop.pdb')
-        rm.manager.add_motif('resources/motifs/GGAA_tetraloop.pdb')
-        m1 = rm.manager.get_motif(name='GAAA_tetraloop')
-        m2 = rm.manager.get_motif(name='GGAA_tetraloop')
-        ss.add_motif(m1.secondary_structure, m1.name)
-        ss.add_motif(m2.secondary_structure, m2.name)
-        ss_m = ss.motif('GGAA_tetraloop')
-        last_end = None
-        for i, end in enumerate(ss_m.ends):
-            if ss_m.end_ids[i] == 'GGGAAC_LUUUUR_CCUGUGUC_LLULUULL_GAAUCUGG_RRUURURR':
-                last_end = end
-                break
-        conn = ss.motif_topology_from_end(ss.ends[1], last_end=last_end)
-        mtt = motif_tree_topology.MotifTreeTopology(conn)
-        mt = motif_tree.motif_tree_from_topology(mtt, sterics=0)
-        #mt.write_pdbs()
 
 
 class MotifUnittest(unittest.TestCase):
@@ -225,7 +192,7 @@ class PoseUnittest(unittest.TestCase):
         p = ssf.factory.pose("AGCUAGG+CCAGCU",
                              "((((.((+))))))")
         p.build_helices()
-        print len(p.helices)
+        self.failIf(len(p.helices) != 2, "did not build the right number of helices")
 
 
 def main():
