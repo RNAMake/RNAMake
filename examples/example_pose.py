0a5d6201971c2845dc010930ea3761dc95bce2a6
Joseph Yesselman <jyesselm@stanford.edu>
2016-05-12 10:35:59 -0700

    redid basic examples, other ones need to be redone

--- examples/example_pose.py
+++ examples/example_pose.py
@@ -1,11 +1,10 @@
-import rnamake.pose_factory as pf
-import rnamake.motif_type as motif_type
+from rnamake import pose_factory, motif_type
 
 #A pose is a RNA structure that contains more then one motif. This is not
 #rigidly enforced, but does allow for more functionality for larger structures
 #like a motif, a pose can be loaded from a preformed directory or from a pdb
 
-p = pf.factory.pose_from_file("resources/p4p6")
+p = pose_factory.factory.pose_from_file("resources/p4p6")
 
 print "this structure contains", len(p.motifs(motif_type.TWOWAY)), "twoway junctions"
 print "visualize them with pymol twoways.*"
@@ -20,10 +19,11 @@ print "#########################################################################
 print "p.optimized_sequence() => get optimized sequence"
 #print p.optimized_sequence()
 
+print "printing all motifs, with type, sequence and secondary structure"
 for m in p.motifs(motif_type.ALL):
     if m.mtype == motif_type.HELIX:
         continue
-    print m.sequence(), m.dot_bracket()
+    print motif_type.type_to_str(m.mtype), m.sequence(), m.dot_bracket()
 
 
 
