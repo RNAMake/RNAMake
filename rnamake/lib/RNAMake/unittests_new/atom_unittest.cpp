c0dd241bf33935a18fdd8110950b3bafd892a26d
Joseph Yesselman <jyesselm@stanford.edu>
2016-05-29 17:55:17 -0700

    translated first test to catch, way prettier

--- rnamake/lib/RNAMake/unittests_new/atom_unittest.cpp
+++ rnamake/lib/RNAMake/unittests_new/atom_unittest.cpp
@@ -1,5 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "is_equal.hpp"
 
 #include "structure/atom.h"
 
@@ -11,13 +12,34 @@ TEST_CASE( "Test Atoms for Structure", "[Atom]" ) {
     REQUIRE(a->coords() == p);
     REQUIRE(a->name() == "P");
     
-    SECTION("producing the correct pdb output") {
+    SECTION("do atoms have correct pdb output") {
         auto s = a->to_pdb_str(1);
-        auto ref = "ATOM      1  P   C   A   1       0.000   1.000   3.000  1.00 62.18           P\n";
+        auto ref = "ATOM      1  P   C   A   1       0.000   1.000   2.000  1.00 62.18           P\n";
         
         REQUIRE(s == ref);
+        
+        auto s1 = a->to_pdb_str(10);
+        auto ref1 = "ATOM     10  P   C   A   1       0.000   1.000   2.000  1.00 62.18           P\n";
+        
+        REQUIRE(s1 == ref1);
     }
     
+    
+    SECTION("do atoms stringify properly") {
+        auto s = a->to_str();
+        auto a2 = std::make_shared<Atom>(s);
+        
+        REQUIRE(s == "P 0 1 2");
+        REQUIRE(are_atoms_equal(a, a2));
+        
+    }
+    
+    SECTION("do atoms copy properly") {
+        auto a = std::make_shared<Atom>("P", Point(0, 1, 2));
+        auto a2 = std::make_shared<Atom>(*a);
+
+        REQUIRE(are_atoms_equal(a, a2));
+    }
     
 }
 
