9073bf3782a666a30353975c5dd9bb336ce5e65e
Joseph Yesselman <jyesselm@stanford.edu>
2016-06-07 12:10:13 -0700

    added missing files

---
+++ rnamake/lib/RNAMake/src/base/file_io.cc
@@ -0,0 +1,30 @@
+//
+//  FileIO.cpp
+//  REDESIGNC
+//
+//  Created by Joseph Yesselman on 9/29/14.
+//  Copyright (c) 2014 Joseph Yesselman. All rights reserved.
+//
+
+#include "base/file_io.h"
+
+Strings
+get_lines_from_file(String const fname) {
+    if(!file_exists(fname)) {
+        std::cout << "File: " << fname << "does not exists" << std::endl;
+        throw "file Does not Exist";
+    }
+    
+    String line;
+    Strings lines;
+    std::ifstream input;
+    input.open(fname);
+    while ( input.good() ) {
+        getline(input, line);
+        lines.push_back(line);
+        
+    }
+    input.close();
+    
+    return lines;
+}
