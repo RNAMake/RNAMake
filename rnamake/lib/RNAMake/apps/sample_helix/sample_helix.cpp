96c932a61cbdf80bcda148ba5cea070b6ed5748e
Joseph Yesselman <jyesselm@stanford.edu>
2016-06-07 12:08:57 -0700

    added sample helix application

---
+++ rnamake/lib/RNAMake/apps/sample_helix/sample_helix.cpp
@@ -0,0 +1,19 @@
+//
+//  sample_helix.cpp
+//  RNAMake
+//
+//  Created by Joseph Yesselman on 6/7/16.
+//  Copyright © 2016 Joseph Yesselman. All rights reserved.
+//
+
+#include "sample_helix.hpp"
+
+#include "base/cl_option.h"
+
+
+
+int main(int argc, const char * argv[]) {
+
+    
+    return 0;
+}
---
+++ rnamake/lib/RNAMake/apps/sample_helix/sample_helix.hpp
@@ -0,0 +1,14 @@
+//
+//  sample_helix.hpp
+//  RNAMake
+//
+//  Created by Joseph Yesselman on 6/7/16.
+//  Copyright © 2016 Joseph Yesselman. All rights reserved.
+//
+
+#ifndef sample_helix_hpp
+#define sample_helix_hpp
+
+#include <stdio.h>
+
+#endif /* sample_helix_hpp */
