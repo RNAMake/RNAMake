d17e92b5f06786c8e9d220ff9c8f08a0af2bda74
Joseph Yesselman <jyesselm@stanford.edu>
2016-06-07 12:08:57 -0700

    added sample helix application

--- rnamake/lib/RNAMake/cmake/build/base.cmake
+++ rnamake/lib/RNAMake/cmake/build/base.cmake
@@ -1,7 +1,9 @@
 set(base_files
 	../../src/base/base.cc
 	../../src/base/cl_option.cc
+	../../src/base/file_io.cc
 	../../src/base/option.cc
+	../../src/base/settings.cc
 	../../src/base/string.cc
 	../../src/base/variant.cc
 )
--- rnamake/lib/RNAMake/cmake/build/structure.cmake
+++ rnamake/lib/RNAMake/cmake/build/structure.cmake
@@ -3,6 +3,7 @@ set(structure_files
 	../../src/structure/basepair.cc
 	../../src/structure/basepair_state.cc
 	../../src/structure/chain.cc
+	../../src/structure/is_equal.cpp
 	../../src/structure/pdb_parser.cc
 	../../src/structure/residue.cc
 	../../src/structure/residue_type.cc
