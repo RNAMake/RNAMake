d8cd71f1beea3bdd356ed2231f38eeb672a83e80
Andy Watkins <amw579@nyu.edu>
2016-07-12 14:35:43 -0700

    Beautifying Vienna code with clang-format

--- rnamake/lib/RNAMake/cmake/build/cmake_install.cmake
+++ rnamake/lib/RNAMake/cmake/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/josephyesselman/projects/RNAMake/rnamake/lib/RNAMake/cmake/build
+# Install script for directory: /Users/amw579/RNAMake/rnamake/lib/RNAMake/cmake/build
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/josephyesselman/projects/RNAMake/rnamake/lib/RNAMake/cmake/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/amw579/RNAMake/rnamake/lib/RNAMake/cmake/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
