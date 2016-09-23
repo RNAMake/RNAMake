61fb112d3c511b546a6a1aed4936736cfe10847f
Joseph Yesselman <jyesselm@stanford.edu>
2016-09-23 14:11:37 -0700

    another minor fix suggested by Andy

--- compile.py
+++ compile.py
@@ -1,12 +1,16 @@
 import os
 import subprocess
+import shutil
 from rnamake import settings, util
 
 path = util.base_dir(os.path.realpath(__file__))
 
-
-
 os.chdir(settings.LIB_PATH + "/lib/RNAMake/cmake/build")
+
+if not os.path.isfile("build.ninja") and os.path.isfile("CMakeCache.txt"):
+    os.remove("CMakeCache.txt")
+    shutil.rmtree("CMakeFiles")
+
 subprocess.call("python make_project.py", shell=True)
 subprocess.call("cmake -G Ninja", shell=True)
 subprocess.call("ninja", shell=True)
