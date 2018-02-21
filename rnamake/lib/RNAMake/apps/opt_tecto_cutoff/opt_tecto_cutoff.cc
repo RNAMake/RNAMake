051c9500283c79d452aa620f36d7ca841ce16895
Joseph Yesselman <jyesselm@stanford.edu>
2018-02-20 21:45:01 -0800

    add files 2

---
+++ rnamake/lib/RNAMake/apps/opt_tecto_cutoff/opt_tecto_cutoff.cc
@@ -0,0 +1,338 @@
+//
+// Created by Joseph Yesselman on 2/19/18.
+//
+
+#include "base/backtrace.hpp"
+#include "math/hashing.h"
+#include "math/stats.h"
+#include "util/monte_carlo.h"
+#include "util/random_number_generator.h"
+#include "base/file_io.h"
+#include "opt_tecto_cutoff/opt_tecto_cutoff.h"
+
+
+OptTectoCutoff::OptTectoCutoff() : Application() {}
+
+// application setups functions ////////////////////////////////////////////////////////////////////
+
+void
+OptTectoCutoff::setup_options() {
+    //add_option("pdb", "", OptionType::STRING, true);
+    add_option("histos", "", OptionType::STRING, true);
+    add_option("data", "", OptionType::STRING, true);
+    add_option("divide_set", false, OptionType::BOOL, false);
+    add_option("divide_set_num", 10, OptionType::INT, false);
+    add_option("constraints", "", OptionType::STRING, false);
+
+
+}
+
+void
+OptTectoCutoff::parse_command_line(
+        int argc,
+        const char ** argv) {
+    Application::parse_command_line(argc, argv);
+}
+
+
+void
+OptTectoCutoff::_setup() {
+
+    auto lines = get_lines_from_file(get_string_option("data"));
+    exp_dgs_ = std::vector<double>();
+    constructs_ = Constructs();
+    for(int i = 1; i < lines.size(); i++) {
+        auto spl = split_str_by_delimiter(lines[i], ",");
+        if(spl.size() < 3) { break; }
+        constructs_.push_back(Construct{spl[0], spl[1], spl[2], spl[3], std::stod(spl[4]), i-1});
+        exp_dgs_.push_back(std::stod(spl[4]));
+        //if(exp_dgs_.size() > 100) { break; }
+    }
+
+    auto in = std::ifstream();
+    in.open(get_string_option("histos"), std::ios::binary);
+    histos_ =  std::vector<SixDHistogram>();
+    while (in.good()) {
+        histos_.push_back(SixDHistogram(in));
+        if(histos_.back().size() == 0) {
+            exp_dgs_.erase(exp_dgs_.begin()+histos_.size()-1);
+            constructs_.erase(constructs_.begin()+histos_.size()-1);
+            histos_.pop_back();
+        }
+        if(histos_.size() == constructs_.size()) { break; }
+    }
+    in.close();
+
+    std::cout << histos_.size() << " " << exp_dgs_.size() << std::endl;
+
+    avg_hit_counts_ = std::vector<double>(histos_.size());
+    pred_dgs_ = std::vector<double>(histos_.size());
+    norm_exp_dgs_ = std::vector<double>(histos_.size());
+
+}
+
+void
+OptTectoCutoff::_divide_dataset() {
+    std::sort(constructs_.begin(), constructs_.end(), CompareConstructDG());
+
+    int step = get_int_option("divide_set_num");
+
+    for(int j = 0; j < step; j++) {
+        auto c_out = std::ofstream("constructs_"+std::to_string(j) + ".csv");
+        auto h_out = std::ofstream("histos_"+std::to_string(j) + ".out", std::ios::binary);
+        c_out << "fseq,fss,cseq,css,dG\n";
+        int i = 0;
+        for(i = 0; i < constructs_.size(); i+=step) {
+            if(histos_[constructs_[i+j].pos].size() == 0) { continue; }
+            c_out << constructs_[i+j].fseq << "," << constructs_[i+j].fss << ",";
+            c_out << constructs_[i+j].cseq << "," << constructs_[i+j].css << ",";
+            c_out << constructs_[i+j].dg << std::endl;
+            histos_[constructs_[i+j].pos].output_binary(h_out, -1);
+        }
+        if(i+j < constructs_.size()) {
+            c_out << constructs_[i+j].fseq << "," << constructs_[i+j].fss << ",";
+            c_out << constructs_[i+j].cseq << "," << constructs_[i+j].css << ",";
+            c_out << constructs_[i+j].dg << std::endl;
+            histos_[constructs_[i+j].pos].output_binary(h_out, -1);
+        }
+        c_out.close();
+        h_out.close();
+    }
+
+}
+
+void
+OptTectoCutoff::_get_scored_dataset() {
+    auto constraints_str = get_string_option("constraints");
+
+    auto spl =  split_str_by_delimiter(constraints_str, ";");
+    for(auto const & s : spl) {
+        auto spl2 = split_str_by_delimiter(s, ",");
+        auto pos = _parse_constraint_position(spl2[0]);
+        auto lower = std::stod(spl2[1]);
+        auto upper = std::stod(spl2[2]);
+        constraints_[pos] = Real2{lower, upper};
+    }
+
+    auto score = _score(constraints_);
+
+    std::cout << score << " " << r_ << " " << avg_diff_ << std::endl;
+
+    auto out = std::ofstream();
+    out.open("scored.csv");
+    out << "fseq,fss,cseq,css,dG,norm_dG,predicted_dG\n";
+    int i = 0;
+    for(auto const & c : constructs_) {
+        out << c.fseq << "," << c.fss << "," << c.cseq << "," << c.css << ",";
+        out << c.dg << "," << norm_exp_dgs_[i] << "," << pred_dgs_[i] << std::endl;
+        i++;
+    }
+    out.close();
+
+}
+
+
+int
+OptTectoCutoff::_parse_constraint_position(
+        String const & constraint_name) {
+    if     (constraint_name == "x") { return 0; }
+    else if(constraint_name == "y") { return 1; }
+    else if(constraint_name == "z") { return 2; }
+    else if(constraint_name == "a") { return 3; }
+    else if(constraint_name == "b") { return 4; }
+    else if(constraint_name == "g") { return 5; }
+    else {
+        throw std::runtime_error("unknown constraint name: " + constraint_name);
+    }
+
+}
+
+
+
+double
+OptTectoCutoff::_score(
+        std::array<Real2, 6> const & constraints) {
+
+    for(int i = 0; i < histos_.size(); i++) {
+        avg_hit_counts_[i] = histos_[i].within_constraints(constraints);
+        if(avg_hit_counts_[i] < 1) { return 10000; }
+    }
+    auto avg_hit_count_mean = mean(avg_hit_counts_);
+
+
+    double closest = 1000;
+    int closest_i = 0;
+    double diff = 0;
+    for(int i = 0; i < histos_.size(); i++) {
+        diff = abs(avg_hit_counts_[i] - avg_hit_count_mean);
+        if(diff < closest) {
+            closest = diff;
+            closest_i = i;
+        }
+    }
+
+    for(int i = 0; i < histos_.size(); i++) {
+        pred_dgs_[i] = 1.9872041e-3*298*log(avg_hit_counts_[closest_i] / avg_hit_counts_[i]);
+        norm_exp_dgs_[i] = exp_dgs_[i] - exp_dgs_[closest_i];
+    }
+
+    r_ = pearson_coeff(pred_dgs_, norm_exp_dgs_);
+    avg_diff_ =  avg_unsigned_diff(pred_dgs_, norm_exp_dgs_);
+    return  1.0/r_ + avg_diff_*2;
+
+}
+
+void
+OptTectoCutoff::_vary_constraints(
+        std::array<Real2, 6> const & constraints,
+        std::array<Real2, 6> & new_constraints,
+        RandomNumberGenerator & rng) {
+
+    new_constraints = constraints;
+    double val = 0;
+    bool done = false;
+
+    while(! done) {
+        auto pos = rng.randrange(6);
+        auto i = rng.randrange(2);
+        auto diff = 1 + rng.randrange(3);
+        auto dir = rng.randrange(2);
+
+        if (pos < 3) { val = 0.1 * diff; }
+        else { val = 2.5 * diff; }
+
+        if (dir == 1) { val = -val; }
+
+        if (pos < 3) {
+            if(new_constraints[pos][i] + val > 10 || new_constraints[pos][i] + val < -10) { continue; }
+        }
+        else {
+            if(new_constraints[pos][i] + val > 360 || new_constraints[pos][i] + val < 0) { continue; }
+        }
+
+        new_constraints[pos][i] += val;
+        break;
+    }
+
+
+}
+
+
+void
+OptTectoCutoff::_get_initial_constraints(
+        RandomNumberGenerator & rng) {
+    constraints_ = std::array<Real2, 6>();
+
+    bool done = false;
+    while(!done) {
+        for (int i = 0; i < 6; i++) {
+            if (i < 3) {
+                auto min = -rng.randrange(70) * .10;
+                auto max = rng.randrange(70) * .10;
+                constraints_[i][0] = min;
+                constraints_[i][1] = max;
+            }
+            else {
+                auto min = -rng.randrange(72) * 2.5 + 180;
+                auto max = rng.randrange(72) * 2.5 + 180;
+                constraints_[i][0] = min;
+                constraints_[i][1] = max;
+            }
+        }
+        auto score = _score(constraints_);
+        for(auto const & c : constraints_) { std::cout << c[0] << " " << c[1] << " "; }
+        std::cout << std::endl;
+        if(score != 10000) { return; }
+    }
+
+}
+
+void
+OptTectoCutoff::run() {
+    _setup();
+    if(get_bool_option("divide_set")) {
+        _divide_dataset();
+        return;
+    }
+    if(get_string_option("constraints") != "") {
+        _get_scored_dataset();
+        return;
+    }
+
+    auto rng = RandomNumberGenerator();
+    auto mc = MonteCarlo(0.05);
+    _get_initial_constraints(rng);
+
+    //constraints_ = {Real2{-5, 5}, Real2{-5, 5}, Real2{-5, 5}, Real2{10, 340}, Real2{130, 190}, Real2{170, 190}};
+    //constraints_ = {Real2{-4.5, 4.0}, Real2{-4.5, 4.5}, Real2{-4.5, 4.5}, Real2{10, 340}, Real2{150, 190}, Real2{180, 200}};
+    //constraints_ = {Real2{-4.5, 4.0}, Real2{-4.5, 4.5}, Real2{-4.5, 4.5}, Real2{10, 340}, Real2{150, 190}, Real2{140, 200}};
+
+    new_constraints_ = std::array<Real2, 6>();
+
+
+    auto current_score = _score(constraints_);
+    auto new_score = current_score;
+
+    std::cout << current_score << " " << r_ << " " << avg_diff_ << std::endl;
+
+    double best_score = 10000;
+    auto best_constraints = constraints_;
+    double best_avg_diff;
+    double best_r;
+
+    int heat_up = 0;
+    for(int i = 0; i < 10000; i++) {
+        if( i % 100 == 0) {
+            constraints_ = best_constraints;
+            current_score = best_score;
+            std::cout << "STEPS: " << i << std::endl;
+            heat_up = 0;
+            mc.set_temperature(0.5);
+        }
+
+        heat_up += 1;
+        if(heat_up > 20) {
+            mc.set_temperature(0.05);
+        }
+
+        _vary_constraints(constraints_, new_constraints_, rng);
+        new_score = _score(new_constraints_);
+        if(mc.accept(current_score, new_score)) {
+            constraints_ = new_constraints_;
+            current_score = new_score;
+            if(best_score > current_score) {
+                best_score = current_score;
+                best_constraints = constraints_;
+                best_r = r_;
+                best_avg_diff = avg_diff_;
+                std::cout << "best: " << best_score << " " << best_r << " " << best_avg_diff << std::endl;
+                for(auto const & c : best_constraints) {
+                    std::cout << c[0] << " " << c[1] << " ";
+                }
+                std::cout << std::endl;
+            }
+        }
+
+
+
+    }
+
+
+
+
+}
+
+
+int main(int argc, const char * argv[]) {
+    //must add this for all apps!
+    std::set_terminate(print_backtrace);
+
+    auto app = OptTectoCutoff();
+    app.setup_options();
+    app.parse_command_line(argc, argv);
+    app.run();
+
+    return 0;
+
+
+}
