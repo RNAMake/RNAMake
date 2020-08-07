219194cec6bc6b4c88dbc20c862e8ada338f8b02
Chris Jurich <cjurich2@huskers.unl.edu>
2020-08-07 18:16:52 -0500

    hopefully backtracke works now

--- cmake/build/backtrace.cmake
+++ cmake/build/backtrace.cmake
@@ -8,15 +8,15 @@ set_source_files_properties(
             PROPERTIES
             LANGUAGE C
             )
-MESSAGE(${COMPILE_FLAGS})
+
 set(OLD_FLAGS ${COMPILE_FLAGS})
 
-set(COMPILE_FLAGS "-O2 -shared -Wall -lbfd -liberty -limagehlp -lz -lintl")
+set(COMPILE_FLAGS "-O2 -shared -o -Wall -lbfd -liberty -limagehlp -lz -lintl")
 
-MESSAGE(${COMPILE_FLAGS})
+
 add_library(backtrace ${backtrace_src})
-target_link_libraries(backtrace_src)
 
-set(COMPILE_FLAGS OLD_FLAGS)
+target_link_libraries(backtrace)
 
-MESSAGE(COMPILE_FLAGS)
+set(COMPILE_FLAGS ${OLD_FLAGS})
+
