8a3330a9b794c728f6cf7efaac30a8028a337e4d
Chris Jurich <cjurich2@huskers.unl.edu>
2020-08-10 19:24:18 -0500

    -pthread for linux build

--- cmake/make_project.py
+++ cmake/make_project.py
@@ -125,7 +125,8 @@ if __name__ == '__main__':
     elif args.target == "windows":
         static = True
     elif args.target == "linux":
-        static=True #TODO this needs to change
+        pass
+        #static=True #TODO this needs to change
     else:
         raise Exception("Invalid value for \"-target\" flag. Acceptable values are \"mac\",\"windows\" and \"linux\"")
 
