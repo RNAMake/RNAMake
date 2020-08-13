089567b82ecc42e66fb2d533460aa5b8d8b10531
Chris Jurich <cjurich2@huskers.unl.edu>
2020-08-13 11:24:10 -0500

    build changes

--- cmake/build/sqlite.cmake
+++ cmake/build/sqlite.cmake
@@ -20,7 +20,7 @@ set_source_files_properties(
 
 if (CMAKE_SYSTEM_NAME STREQUAL "Linux")
     add_library(sqlite3 STATIC ../../src/external/sqlite/sqlite3.c )
-    target_link_libraries(sqlite3 -static)
+    target_link_libraries(sqlite3 -static ${CMAKE_DL_LIBS})
 
 else()
 
