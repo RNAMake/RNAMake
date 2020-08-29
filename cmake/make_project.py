e5fd2506f68ded311f96871941b4323d9907b87e
Chris Jurich <cjurich2@huskers.unl.edu>
2020-08-29 10:46:05 -0500

    standard = C++17

--- cmake/make_project.py
+++ cmake/make_project.py
@@ -98,7 +98,8 @@ def build_header(base_dir,static,target):
     # header_contents+= "set(CMAKE_HOST_SYSTEM_VERSION 2.5)\n"
     header_contents+="""include({CMAKE})
 set(RNAMAKE {RNAMAKE})
-include({SQLITE})
+set(RNAMAKE {RNAMAKE})
+set(CMAKE_CXX_STANDARD 17)
 include_directories({BASE})
 include_directories({EXTERN})
 include_directories({UNITTESTS})
