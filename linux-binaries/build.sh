d6200ec8983f70a41cf3335c95029b32e7c571fc
Chris Jurich <cjurich2@huskers.unl.edu>
2020-09-08 19:57:03 -0500

    fixed linux build

---
+++ linux-binaries/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone --branch cjurich/linux-binaries https://chrisjurich:Ilikesoccer5\!@github.com/RNAMake/RNAMake.git && \
+    export RNAMAKE=/RNAMake/ && \
+    export X3DNA=$RNAMAKE/resources/x3dna/linux/ && \
+    cd RNAMake/cmake/build/ && \
+    python ../make_project.py -target mac && \
+    cmake -G Ninja
+    time ninja
+    cd /
+    # rm -rf -f *.a CMakeCache.txt CMakeFiles/ *.txt *.cmake ../../src/ ../../.git* && \
+    #cd / && \
+    tar -cvzf RNAMake-binaries.tar.gz RNAMake/
