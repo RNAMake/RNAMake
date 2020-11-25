e39ce3f63b3a68989379167890e104786fe2ba11
cjurich <cjurich2@huskers.unl.edu>
2020-11-24 20:23:06 -0600

    added rnamake2.fwd.hh

---
+++ src/rnamake2d/rnamake2d.fwd.hh
@@ -0,0 +1,16 @@
+#ifndef RNAMAKE2D_FWD_DECLARE_H
+#define RNAMAKE2D_FWD_DECLARE_H
+
+
+namespace rnamake2d {
+
+   class NemoSampler;
+
+   class Design;
+
+} // namespace rnamake2d
+
+#include <rnamake2d/design.h>
+#include <rnamake2d/NemoSampler.h>
+
+#endif //RNAMAKE2D_FWD_DECLARE_H
