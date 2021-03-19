22df33a990252fdd7921c63f124e1465c1122173
Hassan Abdelsamad <hassanbakry2010@gmail.com>
2021-03-19 12:38:58 -0500

    Some changes to x3dna

--- src/util/find_pair.h
+++ src/util/find_pair.h
@@ -9,56 +9,59 @@
 namespace util
 {
 
-    class PairFinder
-    {
-    public:
-        PairFinder(std::string pdb);
+class PairFinder
+{
 
-        std::vector<X3dna::X3Basepair>
-        find_pair();
-        
-    private:
-        struct Args
-        {
-            char pdbfile[BUF512];
-            char outfile[BUF512];
-            long ds = 2;
-            long curves = FALSE;
-            long curves_plus = FALSE;
-            long divide = FALSE;
-            long hetatm = TRUE;
-            long pairs = FALSE;
-            long detailed = FALSE;
-            long waters = FALSE;
-            long hjb = FALSE;
-        };
+public:
+    PairFinder(std::string pdb);
 
-        Args args = Args();
+    void
+    find_pair(X3dna::X3Basepairs &basepairs);
+    
+// private functions
+private:
 
-        // X3dna::X3BPInfo *bp_info;
+    void
+    _handle_str();
 
-        void
-        handle_str();
+    void
+    _write_fpmst(double *morg, double *morien, FILE *rframe, X3dna::X3BPInfo *bp_info);
 
-        void
-        write_fpmst(double *morg, double *morien, FILE *rframe, X3dna::X3BPInfo *bp_info);
+    void
+    _write_bestpairs(long num_bp, long **base_pairs, long *bp_idx, char *bseq,
+                            long **seidx, char **AtomName, char **ResName, char *ChainID,
+                            long *ResSeq, char **Miscs, double **xyz, double **orien,
+                            double **org, long **htm_water, miscPars *misc_par);
 
-        void
-        write_bestpairs(long num_bp, long **base_pairs, long *bp_idx, char *bseq,
-                                long **seidx, char **AtomName, char **ResName, char *ChainID,
-                                long *ResSeq, char **Miscs, double **xyz, double **orien,
-                                double **org, long **htm_water, miscPars *misc_par);
+    void
+    _duplex(long num, long num_residue, char *bseq, long **seidx, long *RY,
+                    char **AtomName, char **ResName, char *ChainID, long *ResSeq,
+                    char **Miscs, double **xyz, char *parfile,
+                    miscPars *misc_pars);
 
-        void
-        duplex(long num, long num_residue, char *bseq, long **seidx, long *RY,
-                       char **AtomName, char **ResName, char *ChainID, long *ResSeq,
-                       char **Miscs, double **xyz, char *parfile,
-                       miscPars *misc_pars);
+private:
 
-        char **nt_info;
+    char **nt_info;
 
-        std::vector<X3dna::X3Basepair> bps;
+    std::vector<X3dna::X3Basepair> bps;
+
+    struct Args {
+        char pdbfile[BUF512];
+        char outfile[BUF512];
+        long ds = 2;
+        long curves = FALSE;
+        long curves_plus = FALSE;
+        long divide = FALSE;
+        long hetatm = TRUE;
+        long pairs = FALSE;
+        long detailed = FALSE;
+        long waters = FALSE;
+        long hjb = FALSE;
     };
+
+    Args args = Args();
+};
+
 }
 
 #endif
