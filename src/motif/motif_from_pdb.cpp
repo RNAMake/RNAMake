90b67ab2afe4f31b70e4354e5a267abb64757523
Hassan Abdelsamad <hassanbakry2010@gmail.com>
2021-04-08 12:06:16 -0500

    WIP

--- src/motif/motif_from_pdb.cpp
+++ src/motif/motif_from_pdb.cpp
@@ -1,28 +1,41 @@
+#include <CLI/CLI.hpp>
+
 #include "motif/motif_from_pdb.h"
 #include "motif/motif_factory.h"
-#include "motif"
+#include "motif/motif_ensemble.h"
+#include "motif/motif.h"
+#include "base/backtrace.h"
+#include "base/log.h"
+#include <CLI/CLI.hpp>
+
+#include <CLI/CLI.hpp>
+
 #include <vector>
 #include <filesystem>
 
 using recursive_directory_iterator = std::filesystem::recursive_directory_iterator;
 
-namespace motif {
+motif::MotifOPs motifs_;
 
-    MotifOPs motifs_;
+ComputeEnsemble::ComputeEnsemble() :
+        base::Application(),
+        rm_(resources::Manager::instance()),
+        app_("ComputeEnsemble") {}
 
 void
-compute_motif(String const & path, vector<int> list) {
+compute_motif(String const &path, std::vector<float> list) {
     // Add a for loop
-    for (const auto& file_path : recursive_directory_iterator(path)) {
-        MotifFactory factory = MotifFactory();
-        auto motif = factory.motif_from_file(file_path);
+    for (const auto &file_path_entry : recursive_directory_iterator(path)) {
+        String path_string = file_path_entry.path().string();
+        motif::MotifFactory factory = motif::MotifFactory();
+        auto motif = factory.motif_from_file(path_string);
         motif = factory.align_motif_to_common_frame(motif, 0);
         motifs_.push_back(motif);
     }
 
     // generate ensemble with with a list of energies
     // use to_str for list
-    auto motifEnsemble = MotifEnsemble(motifs_, list);
+    auto motifEnsemble = motif::MotifEnsemble(motifs_, list);
 }
 
 int
@@ -32,7 +45,6 @@ main(int argc, const char **argv) {
     app.setup_options();
     CLI11_PARSE(app.app_, argc, argv);
     app.run();
-
 
     return 0;
 
