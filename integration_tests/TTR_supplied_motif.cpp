22e6556091dacd7ac3ca698214ac9d3f8b1b9257
Hassan Abdelsamad <hassanbakry2010@gmail.com>
2021-04-30 12:52:37 -0500

    Some changes to the tests

--- integration_tests/TTR_supplied_motif.cpp
+++ integration_tests/TTR_supplied_motif.cpp
@@ -2,8 +2,6 @@
 // Created by Hassan Abdelsamad on 4/18/21.
 //
 
-#include <CLI/CLI.hpp>
-
 #include "base/backtrace.h"
 #include "base/log.h"
 #include "design_rna_scaffold/design_rna_scaffold.h"
@@ -19,24 +17,20 @@ int compareFile(FILE *fPtr1, FILE *fPtr2) {
     auto col = 0;
 
     do {
-        // Input character from both files
         ch1 = fgetc(fPtr1);
         ch2 = fgetc(fPtr2);
 
-        // Increment line
         if (ch1 == '\n') {
             line += 1;
             col = 0;
         }
 
-        // If characters are not same then return false
         if (ch1 != ch2)
             return -1;
         col += 1;
 
     } while (ch1 != EOF && ch2 != EOF);
 
-    /* If both files have reached end */
     if (ch1 == EOF && ch2 == EOF)
         return 1;
     else
@@ -44,7 +38,7 @@ int compareFile(FILE *fPtr1, FILE *fPtr2) {
 }
 
 
-TEST_CASE ("Test TTR") {
+TEST_CASE ("Test TTR_supplied_motif") {
     auto base_path = base::base_dir() + "/apps/simulate_tectos/resources/";
     resources::Manager::instance().add_motif(base_path + "GAAA_tetraloop");
 
@@ -53,20 +47,33 @@ TEST_CASE ("Test TTR") {
 
     app.setup_options();
     base::init_logging(app.log_level());
-    auto args = "--pdb min_tetraloop_receptor.pdb --start_bp A220-A253 --end_bp A144-A159 --log_level debug --sequences_per_design 1 --designs 10 --search_cutoff 5.0";
+    auto args = "--pdb min_tetraloop_receptor.pdb --start_bp A220-A253 --end_bp A144-A159 --log_level debug --sequences_per_design 1 --designs 3 --search_type mc --motif_path flex_helices,twoway,flex_helices,atp_aptamer,flex_helices,twoway,flex_helices,twoway,flex_helices --dump_pdbs --extra_pdbs inputs/atp_aptamer.pdb --search_cutoff 5";
     app.app_.parse(args);
     app.run();
 
-    SUBCASE ("Compare default.scores") {
-        auto *fp1 = fopen("orig_default.scores", "r");
-        auto *fp2 = fopen("default.scores", "r");
-        CHECK(compareFile(fp1, fp2));
+        SUBCASE ("Compare default.scores") {
+            auto *fp1 = fopen("orig_default.scores", "r");
+            auto *fp2 = fopen("default.scores", "r");
+            CHECK(compareFile(fp1, fp2));
+
+        SUBCASE ("Compare default.out") {
+            auto *fp1 = fopen("orig_default.out", "r");
+            auto *fp2 = fopen("default.out", "r");
+            CHECK(compareFile(fp1, fp2));
+
+        }
+        SUBCASE ("Compare design-0") {
+
+        }
+        SUBCASE ("Compare design-1") {
+
+        }
+        SUBCASE ("Compare design-2") {
+
+        }
+        SUBCASE ("Compare design_w_atp_apt") {
+
+        }
     }
 
-    SUBCASE ("Compare default.out") {
-        auto *fp1 = fopen("orig_default.out", "r");
-        auto *fp2 = fopen("default.out", "r");
-        CHECK(compareFile(fp1, fp2));
-    }
-}
 
--- integration_tests/build_around_protein.cpp
+++ integration_tests/build_around_protein.cpp
@@ -2,8 +2,6 @@
 // Created by Hassan Abdelsamad on 4/28/21.
 //
 
-#include <CLI/CLI.hpp>
-
 #include "base/backtrace.h"
 #include "base/log.h"
 #include "design_rna_scaffold/design_rna_scaffold.h"
@@ -12,7 +10,37 @@
 
 #include "common.hpp"
 
-TEST_CASE ("rebuild_p4_p6") {
+int compareFile(FILE *fPtr1, FILE *fPtr2) {
+    char ch1, ch2;
+    auto line = 1;
+    auto col = 0;
+
+    do {
+        // Input character from both files
+        ch1 = fgetc(fPtr1);
+        ch2 = fgetc(fPtr2);
+
+        // Increment line
+        if (ch1 == '\n') {
+            line += 1;
+            col = 0;
+        }
+
+        // If characters are not same then return false
+        if (ch1 != ch2)
+            return -1;
+        col += 1;
+
+    } while (ch1 != EOF && ch2 != EOF);
+
+    /* If both files have reached end */
+    if (ch1 == EOF && ch2 == EOF)
+        return 1;
+    else
+        return -1;
+}
+
+TEST_CASE ("build_around_protein") {
     String base_path = base::base_dir() + "/apps/simulate_tectos/resources/";
     resources::Manager::instance().add_motif(base_path + "GAAA_tetraloop");
 
@@ -29,4 +57,5 @@ TEST_CASE ("rebuild_p4_p6") {
         auto *fp2 = fopen("default.scores", "r");
         CHECK(compareFile(fp1, fp2));
 
+    }
 }
