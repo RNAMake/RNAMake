d7e0eee4722b778de0c77b00abbb09e83cfa114f
Joe Yesselman <jyesselm@unl.edu>
2022-06-13 13:43:42 -0500

    minor formatting changes

--- src/math/rotation.hpp
+++ src/math/rotation.hpp
@@ -9,19 +9,21 @@
 
 namespace math {
 
-inline Matrix3x3 rotation_between_frames(const Matrix3x3 &m1, const Matrix3x3 &m2) {
+inline Matrix3x3 rotation_between_frames(const Matrix3x3 &m1,
+                                         const Matrix3x3 &m2) {
   Matrix3x3 rot = m1.get_transposed() * m2;
   rot.unitarize();
   return rot;
 }
 
-inline void rotation_between_frames(const Matrix3x3 &m1, const Matrix3x3 &m2, Matrix3x3 &rot /* return */) {
+inline void rotation_between_frames(const Matrix3x3 &m1, const Matrix3x3 &m2,
+                                    Matrix3x3 &rot /* return */) {
   rot = m1.get_transposed() * m2;
   rot.unitarize();
 }
 
 // TODO be implemented
-Real difference_between_frames(const Matrix3x3 & m1, const Matrix3x3 & m2) {
+Real difference_between_frames(const Matrix3x3 &m1, const Matrix3x3 &m2) {
   return 0.0f;
 }
 
