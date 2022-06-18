5e110a026afef357a3357d7ad949f9d640609c0f
Joe Yesselman <jyesselm@unl.edu>
2022-06-17 21:51:27 -0500

    minor updates

--- unittests/util/sqlite3_connection_unittest.cpp
+++ unittests/util/sqlite3_connection_unittest.cpp
@@ -106,32 +106,3 @@ TEST_CASE("Test sqlite3 interface ") {
 
   }
 }
-
-/*TEST_CASE( "Test basic connection sqlite3 connection utilty" ) {
-
-    SUBCASE("test catching nonexistant database files") {
-        CHECK_THROWS_AS(util::Sqlite3Connection("test.db"),
-util::Sqlite3ConnectionException);
-    }
-
-    SUBCASE("CHECK a database file to perform query") {
-        auto sql_con = util::Sqlite3Connection();
-        CHECK_THROWS_AS(sql_con.query("SELECT *"),
-util::Sqlite3ConnectionException);
-    }
-
-    auto path = base::resources_path()+"/motif_libraries_new/bp_steps.db";
-    auto sql_con = util::Sqlite3Connection(path);
-
-    SUBCASE("fetch first row of database") {
-        auto row = sql_con.fetch_one("SELECT * from data_table");
-        CHECK(row.size() == 5);
-    }
-
-    SUBCASE("count number of rows in database") {
-        auto count = sql_con.count();
-        CHECK(count > 0);
-
-    }
-
-}      */
