d79416909d0f024da9588fa227d6d66f27b2c6aa
Joe Yesselman <jyesselm@unl.edu>
2022-06-20 13:57:58 -0500

    changed how connection works, now is a pairing of two connection points which feels more useable

--- src/data_structure/graph/graph_base.cc
+++ src/data_structure/graph/graph_base.cc
@@ -7,33 +7,45 @@
 namespace data_structure::graph {
 
 // Connection functions ///////////////////////////////////////////////////////
-Index Connection::get_partner(Index index) const {
-  if (index == node_i) {
-    return node_j;
-  } else if (index == node_j) {
-    return node_i;
+const ConnectionPoint &
+Connection::get_partner(const ConnectionPoint &cp) const {
+  if (cp == cp1) {
+    return cp2;
+  } else if (cp == cp2) {
+    return cp1;
   } else {
-    String msg = "cannot call partner if not a node in this edge";
+    String msg = "cannot call partner if not a node in this connection";
+    base::log_and_throw<GraphException>(msg);
+  }
+}
+
+Index Connection::get_partner_index(Index index) const {
+  if (index == cp1.ni) {
+    return cp2.ni;
+  } else if (index == cp2.ni) {
+    return cp1.ni;
+  } else {
+    String msg = "cannot call partner if not a node in this connection";
     base::log_and_throw<GraphException>(msg);
     return -1; // stop warning
   }
 }
 
 Index Connection::get_edge_index(Index index) const {
-  if (index == node_i) {
-    return edge_i;
-  } else if (index == node_j) {
-    return edge_j;
+  if (index == cp1.ni) {
+    return cp1.ei;
+  } else if (index == cp2.ni) {
+    return cp2.ei;
   } else {
-    String msg = "cannot call end_index if not a node in this edge";
+    String msg = "cannot call end_index if not a node in this connection";
     base::log_and_throw<GraphException>(msg);
     return -1; // stop warning
   }
 }
 
 String Connection::get_str() const {
-  return "ni: " + std::to_string(node_i) + " nj: " + std::to_string(node_j) +
-         " ei: " + std::to_string(edge_i) + " ej: " + std::to_string(edge_j);
+  return "ni: " + std::to_string(cp1.ni) + " nj: " + std::to_string(cp1.ei) +
+         " ei: " + std::to_string(cp2.ni) + " ej: " + std::to_string(cp2.ei);
 }
 
 } // namespace data_structure::graph
--- unittests/data_structure/graph_adjacency_list_unittest.cpp
+++ unittests/data_structure/graph_adjacency_list_unittest.cpp
@@ -10,75 +10,138 @@
 TEST_CASE("Test Graph Data Structure ") {
   using namespace data_structure::graph;
   SUBCASE("test basic adjacency list") {
-    int i = 0;
-    AdjList<int, FixedEdges> adj_list;
-    CHECK(adj_list.get_num_nodes() == 0);
+    SUBCASE("test add") {
+      int i = 0;
+      AdjacencyList<int, FixedEdges> adj_list;
+      adj_list.add_node(i, 1);
+      CHECK(adj_list.get_num_connections() == 0);
+      CHECK(adj_list.get_num_nodes() == 1);
+      CHECK(adj_list.get_node_data(0) == 0);
+    }
+    SUBCASE("test change value of data") {
+      int i = 0, j = 1;
+      AdjacencyList<int, FixedEdges> adj_list;
+      adj_list.add_node(i, 1);
+      adj_list.set_node_data(0, j);
+      CHECK(adj_list.get_node_data(0) == 1);
+    }
+    SUBCASE("test add connection") {
+      int i = 0, j = 1;
+      AdjacencyList<int, FixedEdges> adj_list;
+      adj_list.add_node(i, 1);
+      adj_list.add_node(j, 1);
+      adj_list.add_connection(ConnectionPoint{0, 0}, ConnectionPoint{1, 0});
+      CHECK(adj_list.get_num_nodes() == 2);
+      CHECK(adj_list.get_num_connections() == 1);
+      CHECK(adj_list.connection_point_empty(0, 0) == false);
+    }
+    SUBCASE("test remove node") {
+      int i = 0;
+      AdjacencyList<int, FixedEdges> adj_list;
+      adj_list.add_node(i, 1);
+      adj_list.remove_node(0);
+      CHECK(adj_list.get_num_nodes() == 0);
+      CHECK_THROWS_AS(auto n = adj_list.get_node(0), GraphException);
+      CHECK_THROWS_AS(auto d = adj_list.get_node_data(0), GraphException);
+      CHECK_THROWS_AS(adj_list.remove_node(0), GraphException);
+    }
+    SUBCASE("test remove node 2") {
+      int i = 0;
+      AdjacencyList<int, FixedEdges> adj_list;
+      adj_list.add_node(i, 1);
+      adj_list.remove_node(0);
+      i = 5;
+      adj_list.add_node(i, 1);
+      CHECK(adj_list.get_node_data(0) == 5);
+      adj_list.add_node(i, 1);
+      CHECK(adj_list.get_node_data(1) == 5);
+      adj_list.remove_node(0);
+      CHECK(adj_list.get_node_data(1) == 5);
+      adj_list.add_node(i, 1);
+      CHECK(adj_list.get_node_data(2) == 5);
+    }
+    SUBCASE("test remove connection") {
+      int i = 0, j = 1;
+      AdjacencyList<int, FixedEdges> adj_list;
+      adj_list.add_node(i, 1);
+      adj_list.add_node(j, 1);
+      adj_list.add_connection(ConnectionPoint{0, 0}, ConnectionPoint{1, 0});
+      CHECK(adj_list.are_nodes_connected(0, 1));
+      auto const &con1 = adj_list.get_node_connections(0);
+      CHECK(con1.size() == 1);
+      CHECK(con1[0] != nullptr);
+      adj_list.remove_connection(ConnectionPoint{0, 0}, ConnectionPoint{1, 0});
+      CHECK(adj_list.are_nodes_connected(0, 1) == false);
+      auto const &con2 = adj_list.get_node_connections(0);
+      CHECK(con2.size() == 1);
+      CHECK(con2[0] == nullptr);
+    }
+    SUBCASE("test remove node with a connection") {
+      int i = 0, j = 1;
+      AdjacencyList<int, FixedEdges> adj_list;
+      adj_list.add_node(i, 1);
+      adj_list.add_node(j, 1);
+      adj_list.add_connection(ConnectionPoint{0, 0}, ConnectionPoint{1, 0});
+      adj_list.remove_node(0);
+      CHECK_THROWS_AS(auto f = adj_list.connection_point_empty(0, 0),
+                      GraphException);
+      CHECK(adj_list.connection_point_empty(1, 0));
+    }
+  }
+  SUBCASE("test dynamic edges") {
+    int i = 0, j = 1;
+    AdjacencyList<int, DynamicEdges> adj_list;
     adj_list.add_node(i, 0);
-    CHECK(adj_list.get_num_nodes() == 1);
-    CHECK(adj_list.get_num_connections() == 0);
+    adj_list.add_node(j, 0);
+    adj_list.add_connection(ConnectionPoint{0, 0}, ConnectionPoint{1, 0});
   }
-
-  SUBCASE("testing adding nodes and edges") {
-    //auto adj_list = data_structure::graph::FixedEdged_AL<int>();
-    //adj_list.add_node(0, 3);
-    //adj_list.add_node(1, 3);
-
-    //CHECK(adj_list.get_num_nodes() == 2);
-    //CHECK(adj_list.get_num_edges() == 0);
-
-    /*adj_list.add_edge(data_structure::graph::NodeIndexandEdge{0, 0},
-                      data_structure::graph::NodeIndexandEdge{1, 0});
-
-    CHECK(adj_list.get_num_edges() == 1);
-
-    adj_list.remove_node(0);
-
-    CHECK(adj_list.get_num_nodes() == 1);
-    CHECK(adj_list.get_num_edges() == 0);
-
-    adj_list = data_structure::FixedEdged_AL<int>();
-    adj_list.add_node(0, 3);
-    adj_list.add_node(1, 3);
-
-    adj_list.add_edge(data_structure::NodeIndexandEdge{0, 0},
-                      data_structure::NodeIndexandEdge{1, 0});
-
-    adj_list.remove_edge(data_structure::NodeIndexandEdge{0, 0},
-                         data_structure::NodeIndexandEdge{1, 0});
-
-    CHECK(adj_list.get_num_edges() == 0);     */
+  SUBCASE("check add_connection errors") {
+    int i = 0, j = 1;
+    AdjacencyList<int, FixedEdges> adj_list;
+    adj_list.add_node(i, 2);
+    adj_list.add_node(j, 2);
+    adj_list.add_connection(ConnectionPoint{0, 0}, ConnectionPoint{1, 0});
+    // connection point 1 is filled
+    CHECK_THROWS_AS(
+        adj_list.add_connection(ConnectionPoint{0, 0}, ConnectionPoint{1, 0}),
+        GraphException);
+    // connection point 2 is filled
+    CHECK_THROWS_AS(
+        adj_list.add_connection(ConnectionPoint{0, 1}, ConnectionPoint{1, 0}),
+        GraphException);
+    // node 2 does not exist
+    CHECK_THROWS_AS(
+        adj_list.add_connection(ConnectionPoint{2, 1}, ConnectionPoint{1, 1}),
+        GraphException);
+    // edge 2 does not exist
+    CHECK_THROWS_AS(
+        adj_list.add_connection(ConnectionPoint{1, 2}, ConnectionPoint{1, 1}),
+        GraphException);
   }
-
-  /*SUBCASE("test copying") {
-
-    auto adj_list = data_structure::FixedEdged_AL<int>();
-    adj_list.add_node(0, 3);
-    adj_list.add_node(1, 3);
-    adj_list.add_edge(data_structure::NodeIndexandEdge{0, 0},
-                      data_structure::NodeIndexandEdge{1, 0});
-
-    auto adj_list_2 = adj_list;
-
+  SUBCASE("test remove_connection errors") {
+    int i = 0, j = 1;
+    AdjacencyList<int, FixedEdges> adj_list;
+    adj_list.add_node(i, 2);
+    adj_list.add_node(j, 2);
+    adj_list.add_connection(ConnectionPoint{0, 0}, ConnectionPoint{1, 0});
+    CHECK_THROWS_AS(adj_list.remove_connection(ConnectionPoint{0, 1},
+                                               ConnectionPoint{1, 0}),
+                    GraphException);
+  }
+  SUBCASE("test copying") {
+    int i = 0, j = 1;
+    FixedEdged_AL<int> adj_list;
+    adj_list.add_node(i, 3);
+    adj_list.add_node(j, 3);
+    adj_list.add_connection(ConnectionPoint{0, 0}, ConnectionPoint{1, 0});
+    FixedEdged_AL<int> adj_list_2 = adj_list;
     adj_list.remove_node(0);
-
-    CHECK(adj_list_2.get_num_edges() == 1);
+    CHECK(adj_list_2.get_num_connections() == 1);
     CHECK(adj_list_2.get_num_nodes() == 2);
   }
 
-  SUBCASE("test copying 2") {
-    auto adj_list = data_structure::FixedEdged_AL<int>();
-    adj_list.add_node(0, 3);
-    adj_list.add_node(1, 3);
-    adj_list.add_edge(data_structure::NodeIndexandEdge{0, 0},
-                      data_structure::NodeIndexandEdge{1, 0});
+  /*
 
-    auto adj_list_2 = data_structure::FixedEdged_AL<int>(adj_list);
-
-    adj_list.remove_node(0);
-
-    CHECK(adj_list_2.get_num_edges() == 1);
-    CHECK(adj_list_2.get_num_nodes() == 2);
-  }
 
   SUBCASE("test directed") {
     auto adj_list = data_structure::FixedEdged_DAL<int>();
@@ -90,6 +153,8 @@ TEST_CASE("Test Graph Data Structure ") {
     CHECK(adj_list.has_parent(0) == false);
     CHECK(adj_list.has_parent(1) == true);
   }
+
+
 
   SUBCASE("test directed copy") {
     auto adj_list = data_structure::FixedEdged_DAL<int>();
@@ -109,4 +174,15 @@ TEST_CASE("Test Graph Data Structure ") {
 
     CHECK(adj_list_2.get_num_nodes() == 3);
   }  */
+
+  SUBCASE("test directed copying") {
+    int i = 0, j = 1;
+    FixedEdged_DAL<int> adj_list;
+    adj_list.add_node(i, 3);
+    adj_list.add_node(j, 3, 0, ConnectionPoint{0, 0});
+    FixedEdged_DAL<int> adj_list_2 = adj_list;
+    adj_list.remove_node(0);
+    CHECK(adj_list_2.get_num_connections() == 1);
+    CHECK(adj_list_2.get_num_nodes() == 2);
+  }
 }
