b3bdb032299783a79a4cc6bdca96efe24801a131
Joe Yesselman <jyesselm@unl.edu>
2022-06-25 17:59:24 -0500

    added missing files

---
+++ src/structure/all_atom/atom.cc
@@ -0,0 +1,43 @@
+//
+//  atom.cpp
+//  RNAMake
+//
+//  Created by Joseph Yesselman on 12/23/14.
+//  Copyright (c) 2014 Joseph Yesselman. All rights reserved.
+//
+
+// RNAMake Headers
+#include "atom.h"
+#include "math/numerical.h"
+#include "math/xyz_vector.h"
+
+String structure::Atom::get_str() const {
+  return _name + " " + math::vector_to_str(_coords);
+}
+//
+// json::JSON
+// Atom::get_json() const {
+//    return json::Array(name_->get_str(), coords_.get_json());
+//}
+
+String structure::Atom::to_pdb_str(int acount) const {
+
+  char buffer[200];
+  std::sprintf(
+      buffer,
+      "ATOM %6d  P   C   A   1 %11.3f%8.3f%8.3f  1.00 62.18           P\n",
+      acount, _coords[0], _coords[1], _coords[2]);
+  return String(buffer);
+}
+
+// String
+// structure::Atom::get_pdb_str(
+//         int acount) const {
+//
+//     char buffer[200];
+//     std::sprintf(buffer, "ATOM %6d  P   C   A   1
+//     %11.3f%8.3f%8.3f  1.00 62.18           P\n",
+//                  acount, coords_.get_x(), coords_.get_y(), coords_.get_z());
+//     return String(buffer);
+//
+// }
