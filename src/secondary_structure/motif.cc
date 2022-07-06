e67e6465b2e0761df7ae7ccf8798db2f9633d896
Joe Yesselman <jyesselm@unl.edu>
2022-07-06 18:16:23 -0500

    removed some files that were reimplemented in structure

--- src/secondary_structure/motif.cc
+++
@@ -1,9 +0,0 @@
-//
-//  motif.cc
-//  RNAMake
-//
-//  Created by Joseph Yesselman on 7/31/15.
-//  Copyright (c) 2015 Joseph Yesselman. All rights reserved.
-//
-
-#include "motif.h"
--- src/secondary_structure/pose.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Joseph Yesselman on 10/27/17.
-//
-
-#include <secondary_structure/pose.h>
